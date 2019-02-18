66dec4df4fe2a838c24ea1b123b51fb0246cabdd
Jasper V <jv.jasper.verhoeven@gmail.com>
2019-02-18 03:42:06 +0000

    Uncomment the seedDB function

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set("view engine", "ejs");
 app.use(flash());
 
 // Seed the database, uncomment after initial seeding for persistence
-seedDB();
+// seedDB();
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
